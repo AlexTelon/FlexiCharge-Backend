0f65653fe013669f5ec5e691620ab5a869b3c568
Filip Lundh <lufi18vo@student.ju.se>
2021-09-08 23:33:50 +0200

    Cognito sign up added

---
+++ backend-app/src/presentation-layer/Auth/auth.js
@@ -0,0 +1,41 @@
+var express = require('express')
+var router = express.Router()
+const bodyParser = require('body-parser')
+
+const CognitoService = require('../services/cognito.config')
+
+router.get('/', (request, response) => {
+    response.send('AuthZzzzZZzzzzz')
+})
+
+router.post('/sign-up', function (req, res) {
+
+    console.log(req.body);
+    const { username, password, email, name, family_name } = req.body;
+    const cognito = new CognitoService();
+
+    let userAttributes = [];
+    userAttributes.push({ Name: 'email', Value: email });
+    userAttributes.push({ Name: 'name', Value: name });
+    userAttributes.push({ Name: 'family_name', Value: family_name });
+
+    cognito.signUpUser(username, password, userAttributes)
+        .then(result => {
+            if (result) {
+                console.log(result);
+                res.status(200).end()
+            } else {
+                console.log(result);
+                res.status(500).end()
+            }
+        });
+})
+
+router.get('/sign-in', function (req, res) {
+    res.send('sign-in')
+})
+router.get('/verify', function (req, res) {
+    res.send('verify')
+})
+
+module.exports = router
--- backend-app/src/presentation-layer/app.js
+++ backend-app/src/presentation-layer/app.js
@@ -1,10 +1,17 @@
 const express = require("express")
 const app = express()
+const bodyParser = require('body-parser')
+
 const chargers = require('./Chargers/chargers')
 const transactions = require('./Transactions/transactions')
 const reservations = require('./Reservations/reservations')
+const auth = require('./Auth/auth.js')
 
 module.exports = function ({ databaseTestPresentation }) {
+
+
+    app.use(bodyParser.urlencoded({ extended: false }))
+    app.use(bodyParser.json())
 
     app.get('/', (req, res) => {
         res.send('Helloo')
@@ -13,6 +20,7 @@ module.exports = function ({ databaseTestPresentation }) {
     app.use('/chargers', chargers)
     app.use('/transactions', transactions)
     app.use('/reservations', reservations)
+    app.use('/auth', auth)
 
     app.use('/database', databaseTestPresentation)
 
