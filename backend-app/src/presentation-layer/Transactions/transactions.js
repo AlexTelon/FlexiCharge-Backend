6593fd6dc60a539aafd5475d0632fa86a0bf9125
Bahjazz <33488207+Bahjazz@users.noreply.github.com>
2021-09-09 11:03:05 +0200

    some changes made in transactions

--- backend-app/src/presentation-layer/Transactions/transactions.js
+++ backend-app/src/presentation-layer/Transactions/transactions.js
@@ -5,4 +5,14 @@ router.get('/', function (req, res) {
     res.send('Transactions')
 })
 
+router.post('/', function(req, res){
+    res.send("add transactions")
+})
+
+router.delete('/:id', function (req, res){
+    res.send("delete transactions" )
+})
+router.put(':id', function(req,res){
+    res.send("update transaction with id")
+})
 module.exports = router
