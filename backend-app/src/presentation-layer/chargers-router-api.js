5880a22402a03df764875d557f896127981e6c2f
Annie Johansson <joan19gn@student.ju.se>
2021-09-14 10:38:33 +0200

    started implementing the rest api functions

--- backend-app/src/presentation-layer/chargers-router-api.js
+++ backend-app/src/presentation-layer/chargers-router-api.js
@@ -6,27 +6,86 @@ module.exports = function ({ businessLogicDatabase }) {
     const router = express.Router()
 
     router.get('/', function (req, res) {
-        res.send('Chargers');
+        businessLogicDatabase.getChargers(function (error, chargers) {
+            if (errorCodes.length > 0) {
+                response.status(500).json(error)
+            } else {
+                response.status(200).json(chargers)
+            }
+        }) 
     })
 
     router.get('/:id', function (req, res) {
-        res.send(req.params.id)
+        const id = request.params.id
+        businessLogicDatabase.getCharger(id, function (errors, charger) {
+            if (charger) {
+                response.status(200).json(charger)
+            } else {
+                response.status(404).end()
+            }
+        })
     })
 
-    router.get('/available', function (req, res) {
-        res.senda('return all available chargers');
+    router.get('/available', function(req, res){ 
+        businessLogicDatabase.getAvailableChargers(status, function (errors, charger) {
+            if (status == 1) {
+                response.status(200).json(chargers)
+            } else {
+                response.status(404).end()
+            }
+        })
     })
 
-    router.post('/', function (req, res) {
-        res.send("add charger")
+    router.post('/', function(req, res){
+        const charger = {
+            chargerID: request.body.chargerId,
+            location: request.body.location,
+            chargerPointID: request.body.chargerPointId,
+            status: 0
+        }
+        businessLogicDatabase.addCharger(charger, function (errorCodes) {
+            if (errorCodes.length == 0) {
+                response.status(201).json(charger)
+            } else {
+                if (errorCodes == "internalError") {
+                    response.status(500).end()
+                } else {
+                    response.status(404).end()
+                }
+
+            }
+        })
     })
 
-    router.delete('/:id', function (req, res) {
-        res.send("delete charger")
+    router.delete('/:id', function(req, res){
+        const id = request.params.id
+        businessLogicDatabase.removeCharger(id, function (errors) {
+            if (errors.length == 0) {
+                response.status(204).end()
+            } else {
+                response.status(404).end()
+            }
+        })
     })
 
-    router.put('/:id', function (req, res) {
-        res.send("update charger with id")
+    router.put('/:id', function(req,res){
+        const charger = {
+            chargerId: request.params.id,
+            newLocation: request.body.location,
+            chargerPointID: request.body.chargerPointId,
+            newStatus: request.body.status
+        }
+        businessLogicDatabase.updateChargerStatus(charger, function (errors) {
+            if (!charger) {
+                response.status(404).end()
+            } else {
+                if (errors.length == 0) {
+                    response.status(204).end()
+                } else {
+                    response.status(400).json(errors)
+                }
+            }
+        })
     })
 
 
