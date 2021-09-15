851a4d7ac3972198833b130e874673b4fe0f89e6
Victor Svanqvist <svvi19ty@student.ju.se>
2021-09-15 19:44:13 +0200

    Updated connectedChargers
    Server now removes clients from the chargerconnected array when they disconnect

--- backend-app/src/xOCPP/server_ocpp.js
+++ backend-app/src/xOCPP/server_ocpp.js
@@ -1,11 +1,13 @@
 const WebSocket = require('ws')
 
-const connectedChargers = []
+
+
+
+let connectedChargers = []
 
 module.exports = function({}) {
 
     exports.startServer = function() {
-    
         console.log("Starting OCPP server")
         const wss = new WebSocket.Server({ port: 1337 })
     
@@ -20,9 +22,9 @@ module.exports = function({}) {
                 chargerSerial : ws
             })
 
-            console.log("Number of connected chargers: " + connectedChargers.length)
-    
             console.log("Incoming connection from charger with ID: " + chargerSerial)
+            
+            console.log("Number of connected chargers: " + connectedChargers.length)
     
             ws.on('message', function incoming(message) {
                 var request = JSON.parse(message)
@@ -31,6 +33,12 @@ module.exports = function({}) {
                 console.log("Incoming request call: " + requestType)
     
                 ws.send(JSON.stringify('[3,"call-id",{"status":"Accepted","currentTime":"2019-03-17T05:36:37.760Z","interval":60}]'))
+            })
+
+            ws.on('close', function disconnection(){
+                connectedChargers = connectedChargers.splice(chargerSerial, 1)
+                console.log("Disconnected connection from charger with ID: " + chargerSerial)
+                console.log("Number of connected chargers:" + connectedChargers.length)
             })
         })
     }
