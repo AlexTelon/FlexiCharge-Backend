f130dfb3afc5e778cf42ca9cf76095dc388f2099
EricSoder <70255364+EricSoder@users.noreply.github.com>
2021-09-20 13:39:58 +0200

    Uncommented mock data

--- backend-app/src/data-access-layer/db.js
+++ backend-app/src/data-access-layer/db.js
@@ -106,7 +106,7 @@ Transactions.hasOne(Transactions, { foreignKey: 'chargerID', onDelete: 'cascade'
 Transactions.belongsTo(Chargers, { foreignKey: 'chargerID', onDelete: 'cascade' })
 
 sequelize.sync({ force: true }).then(function() {
-    /*
+
     const chargerOneLocation = {
         type: 'Point',
         coordinates: [57.777725, 14.163085]
@@ -139,7 +139,7 @@ sequelize.sync({ force: true }).then(function() {
         start: 164966755,
         end: 164968555
     });
-    */
+
 });
 
 module.exports = function({}) {
