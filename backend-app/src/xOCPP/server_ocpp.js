bbf1c0da87e329aba99d5a9c5a779c1c3f6bea48
Fredrik Sjölander <f.sjolander@gmail.com>
2021-09-21 08:49:54 +0200

    Added check in DB for connecting charger

--- backend-app/src/xOCPP/server_ocpp.js
+++ backend-app/src/xOCPP/server_ocpp.js
@@ -5,7 +5,7 @@ const WebSocket = require('ws')
 
 const connectedChargers = []
 
-module.exports = function({}) {
+module.exports = function({databaseInterfaceCharger}) {
 
     exports.startServer = function() {
         console.log("Starting OCPP server")
@@ -24,21 +24,39 @@ module.exports = function({}) {
         }
     
         wss.on('connection', function connection(ws, req) {
-            // saving websocket with serial number
+            
+            // Get the charger's serial number:
             let origin = req.url
             let originArray = origin.split("/")
             let chargerSerial = originArray[originArray.length - 1]
 
+            // If connected charger does not pass validation, close connection
+            if(!validateCharger(chargerSerial)){
+                ws.close()
+                console.log("Refused connection from charger '" + chargerSerial + "'. Reason: invalid serial #.")
+            }
+            
+            // Else, save the websocket with the charger's serial in array:
             connectedChargers.push({
                 chargerSerial : ws
             })
-
+            
+            // Log to console
             console.log("Incoming connection from charger with ID: " + chargerSerial)
             console.log("Number of connected chargers: " + connectedChargers.length)
             
-            if(!validateCharger(chargerSerial)){
-                ws.close()
-            }
+            // Check if charger exists in database:
+            databaseInterfaceCharger.getCharger(chargerSerial, function(errorCodes, charger){
+                if (errorCodes.length) {
+                    console.log(errorCodes)
+                } else {
+                    if (charger == null) {
+                        console.log("Charger does not exist in database.")
+                    } else {
+                        console.log("Charger is known since before.")
+                    }
+                }
+            })
 
             ws.on('message', function incoming(message) {
                 var request = JSON.parse(message)
