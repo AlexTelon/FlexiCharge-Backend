8a7d4e052874f2ebaaf13fc6940f54e0d5568d5b
EricSoder <70255364+EricSoder@users.noreply.github.com>
2021-09-21 19:00:30 +0200

    Added a start page with api endpoints

--- backend-app/src/presentation-layer/app.js
+++ backend-app/src/presentation-layer/app.js
@@ -1,12 +1,19 @@
 const express = require("express")
+const expressHandlebars = require('express-handlebars');
 const app = express()
 const bodyParser = require('body-parser')
 
-module.exports = function ({ chargersRouter, transactionsRouter, reservationsRouter, authenticationRouter, databaseTestRouter }) { //authenticationRouter
+module.exports = function({ chargersRouter, transactionsRouter, reservationsRouter, authenticationRouter, databaseTestRouter }) { //authenticationRouter
 
+    app.set('views', '/backend-app/src/presentation-layer/views')
+    app.engine('.hbs', expressHandlebars({ extname: '.hbs' }));
+    app.set('view engine', 'hbs')
+    app.engine("hbs", expressHandlebars({
+        defaultLayout: 'main.hbs'
+    }))
     app.use(bodyParser.urlencoded({ extended: false }))
     app.use(bodyParser.json())
-    app.use(function (request, response, next) {
+    app.use(function(request, response, next) {
         console.log(request.method, request.url);
 
         // response.setHeader("Access-Control-Allow-Origin", "*") // "localhost:3000"
@@ -18,14 +25,14 @@ module.exports = function ({ chargersRouter, transactionsRouter, reservationsRou
     })
 
     app.get('/', (req, res) => {
-        res.send('Helloo')
+        res.render('index.hbs')
     })
 
     app.use('/chargers', chargersRouter)
     app.use('/transactions', transactionsRouter)
     app.use('/reservations', reservationsRouter)
     app.use('/auth', authenticationRouter)
-    
+
     app.use('/database', databaseTestRouter)
 
     return app
