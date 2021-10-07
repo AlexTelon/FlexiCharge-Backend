2d9563009f7886f13d863d6cf2b4bdc35282cebb
annie.miken <55791773+anniemiken@users.noreply.github.com>
2021-10-07 11:48:46 +0200

    removed order_lines from /session funktionen

--- backend-app/src/presentation-layer/transactions-router-api.js
+++ backend-app/src/presentation-layer/transactions-router-api.js
@@ -109,8 +109,7 @@ module.exports = function ({ databaseInterfaceTransactions }) {
     router.post('/session', function (request, response) {
         const userID = request.body.userID
         const chargerID = request.body.chargerID
-        const order_lines = request.body.order_lines
-        databaseInterfaceTransactions.getNewKlarnaPaymentSession(userID, chargerID, order_lines, function (error, klarnaSessionTransaction) {
+        databaseInterfaceTransactions.getNewKlarnaPaymentSession(userID, chargerID, function (error, klarnaSessionTransaction) {
             if (error.length > 0) {
                 response.status(400).json(error)
             } else if (klarnaSessionTransaction) {
