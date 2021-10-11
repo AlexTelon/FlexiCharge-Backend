0f1282c723cd0908dc42f8b7fa35c5241c221eb7
EricSoder <70255364+EricSoder@users.noreply.github.com>
2021-10-11 17:44:52 +0200

    Revert "Merge branch 'LT-main' into 32-remoteStopTransaction"
    This reverts commit ddfd3ed35e5c7bab37ab59922a308c46bbb2c967, reversing
    changes made to c47fe92a5f64fd941ba0b3dd561bf463eae2568f.

--- backend-app/src/data-access-layer/billing/klarna-repository.js
+++ backend-app/src/data-access-layer/billing/klarna-repository.js
@@ -121,9 +121,14 @@ module.exports = function({}) {
         request.end()
     }
 
-    exports.finalizeKlarnaOrder = async function(transaction, transactionId, callback) {
+
+    exports.finalizeKlarnaOrder = async function(transaction, transactionId, klarnaReservationAmount, callback) {
         const newOrderAmount = Math.round(transaction.pricePerKwh * transaction.kwhTransfered);
-        const order_lines = getOrderLines(newOrderAmount)
+        const order_lines = getOrderLines(klarnaReservationAmount)
+
+        order_lines[0].total_amount = newOrderAmount;
+        order_lines[0].unit_price = newOrderAmount;
+
 
         updateOrder(transaction, order_lines, function(error, responseData) {
             if (error.length == 0) {
@@ -139,6 +144,7 @@ module.exports = function({}) {
             }
         })
     }
+
 
     function updateOrder(transaction, order_lines, callback) {
         const data = new TextEncoder().encode(
@@ -214,8 +220,11 @@ module.exports = function({}) {
         }
 
         const request = https.request(captureOptions, result => {
-            if (result.statusCode == 201) {
-                callback([])
+            if (result.statusCode == 200) {
+                result.on('data', jsonResponse => {
+                    responseData = JSON.parse(jsonResponse);
+                    callback([])
+                })
             } else {
                 switch (result.statusCode) {
                     case 403: // Capture not allowed.
@@ -226,6 +235,7 @@ module.exports = function({}) {
                         break;
                     default:
                         callback(["klarnaError"], [])
+
                 }
             }
         })
