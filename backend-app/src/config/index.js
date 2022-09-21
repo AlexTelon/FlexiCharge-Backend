0bed8fbbeca529587e9f5e2758994acd83173b10
khha20 <khha20qz@student.ju.se>
2022-09-21 18:38:59 +0200

    added validations to AWS keys

--- backend-app/src/config/index.js
+++ backend-app/src/config/index.js
@@ -11,5 +11,10 @@ const {error} = validationSchema
 if (error) throw new Error(error);
 
 module.exports = {
-  PORT: !parseInt(process.env.PORT) ? 8080 : process.env.PORT
+  PORT: !parseInt(process.env.PORT) ? 8080 : process.env.PORT,
+  AWS_REGION: process.env.AWS_REGION,
+  AWS_SECRET_HASH: process.env.AWS_SECRET_HASH,
+  AWS_CLIENT_ID: process.env.AWS_CLIENT_ID,
+  AWS_ADMIN_USER_POOL: process.env.AWS_ADMIN_USER_POOL,
+  AWS_USER_POOL: process.env.AWS_USER_POOL
 }
--- backend-app/src/config/validations.js
+++ backend-app/src/config/validations.js
@@ -3,6 +3,11 @@ const joi = require("joi");
 const validationSchema = joi.object()
     .keys({
         PORT: joi.number(),
+        AWS_REGION: joi.string().required(),
+        AWS_SECRET_HASH: joi.string().required(),
+        AWS_CLIENT_ID: joi.string().required(),
+        AWS_ADMIN_USER_POOL: joi.string().required(),
+        AWS_USER_POOL: joi.string().required()
     }).unknown();
 
 module.exports = validationSchema;
