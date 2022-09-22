7ed0aa7bdfcbf013ad857d18c841c7cf523e17a5
Lasse Lejon <lasse.lejon@hotmail.com>
2022-09-22 18:05:23 +0200

    WIP: unit tests for add charger validation.

--- backend-app/src/database-Interface/tests/charger.test.js
+++ backend-app/src/database-Interface/tests/charger.test.js
@@ -1,37 +1,54 @@
 const chargerValidation = require('../validation/chargerValidation')({})
 const {describe, expect, test} = require('@jest/globals')
 
-describe('Charger By SerialNumber Validation', () => {
+describe('Charger By SerialNumber Validation with input:', () => {
     
-    test('with empty string', () => {    
+    test('empty string', () => {    
         const errors = chargerValidation.getChargerBySerialNumberValidation('');
         expect(errors.length).toBe(1);
     });
 
-    test('with null input', () => {
+    test('null', () => {
         const errors = chargerValidation.getChargerBySerialNumberValidation(null);
         expect(errors.length).toBe(1);
     });
 
-    test('with undefined input', () => {
+    test('undefined', () => {
         const valErrors = chargerValidation.getChargerBySerialNumberValidation(undefined);
         expect(valErrors.length).toBe(1);
     });
 
-    test('with int input', () => {
+    test('int', () => {
         const errors = chargerValidation.getChargerBySerialNumberValidation(1);
         expect(errors.length).toBe(1);
     });
 
-    test('with float input', () => {
+    test('float', () => {
         const errors = chargerValidation.getChargerBySerialNumberValidation(11.11);
         expect(errors.length).toBe(1);
     });
 
-    test('with long string', () => {
+    test('long string', () => {
         const errors = chargerValidation.getChargerBySerialNumberValidation(
             'ashdfjasdfj;laksdjflkasjdf;lkjsalkdfjlkajdfljadhfaoiuwoejasklflha;sjdsljdfasdfhjalshdfkja'
         );
         expect(errors.length).toBe(1);
     });
+});
+
+describe('Add Charger Validation with input:', () => {
+    test('all undefined', () => {
+        const errors = chargerValidation.getAddChargerValidation(undefined, undefined, undefined);
+        expect(errors.length).toBe(3);
+    });
+    test('all null', () => {
+        const errors = chargerValidation.getAddChargerValidation(null, null, null);
+        expect(errors.length).toBe(3);
+    });
+    test('all empty string', () => {
+        const errors = chargerValidation.getAddChargerValidation('', '', '');
+        console.log(errors)
+        expect(errors.length).toBe(3);
+    });
+    
 });
