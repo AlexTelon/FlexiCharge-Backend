ff5a2cca8b372ca07ec985d280b3c5c823fff503
Lasse Lejon <lasse.lejon@hotmail.com>
2022-09-22 17:01:21 +0200

    Implemented unit tests for getChargerBySerialNumberValidation

--- backend-app/src/database-Interface/tests/charger.test.js
+++ backend-app/src/database-Interface/tests/charger.test.js
@@ -1,7 +1,37 @@
 const chargerValidation = require('../validation/chargerValidation')({})
 const {describe, expect, test} = require('@jest/globals')
 
-test('what a joke', () => {    
-    const valErrors = chargerValidation.getChargerBySerialNumberValidation('')
-    expect(valErrors.length).toBe(0)
-})
+describe('Charger By SerialNumber Validation', () => {
+    
+    test('with empty string', () => {    
+        const errors = chargerValidation.getChargerBySerialNumberValidation('');
+        expect(errors.length).toBe(1);
+    });
+
+    test('with null input', () => {
+        const errors = chargerValidation.getChargerBySerialNumberValidation(null);
+        expect(errors.length).toBe(1);
+    });
+
+    test('with undefined input', () => {
+        const valErrors = chargerValidation.getChargerBySerialNumberValidation(undefined);
+        expect(valErrors.length).toBe(1);
+    });
+
+    test('with int input', () => {
+        const errors = chargerValidation.getChargerBySerialNumberValidation(1);
+        expect(errors.length).toBe(1);
+    });
+
+    test('with float input', () => {
+        const errors = chargerValidation.getChargerBySerialNumberValidation(11.11);
+        expect(errors.length).toBe(1);
+    });
+
+    test('with long string', () => {
+        const errors = chargerValidation.getChargerBySerialNumberValidation(
+            'ashdfjasdfj;laksdjflkasjdf;lkjsalkdfjlkajdfljadhfaoiuwoejasklflha;sjdsljdfasdfhjalshdfkja'
+        );
+        expect(errors.length).toBe(1);
+    });
+});
