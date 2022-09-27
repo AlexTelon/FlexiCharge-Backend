15a55e69274edfc386ebb8afbd92e3a58885f243
joelAndreasson <joelaron007@gmail.com>
2022-09-27 14:15:44 +0200

    Added "call" switch and "call result" switch

--- backend-app/src/xOCPP/tests/charger_tests.js
+++ backend-app/src/xOCPP/tests/charger_tests.js
@@ -1,22 +1,35 @@
 const WebSocket = require('ws')
 
-module.exports = function ({ ocppInterface, databaseInterfaceCharger, constants, v }) {
+module.exports = function ({ ocppInterface, constants, v, func }) {
     const c = constants.get()  
 
     exports.runTests = function(){
         console.log('\n========= RUNNING TESTS ==========\n')
         const chargerId = 100001
         connectAsChargerSocket(chargerId, function(ws){
-            testRemoteStart(chargerId)
-            testRemoteStop(chargerId)
-            testReserveNow(chargerId)
+            setTimeout(function(){
+                testRemoteStart(chargerId)
+
+                setTimeout(function(){
+                    testRemoteStop(chargerId)
+                }, 2000);
+                
+                setTimeout(function(){
+                    testReserveNow(chargerId)
+                }, 4000);
+
+                setTimeout(function(){
+                    ws.terminate()
+                }, 6000);
+            }, 2000);
+            
         })
         
     }
     
     connectAsChargerSocket = function (chargerId, callback) {
         try {
-            console.log('\n========= CONNECTING... ==========\n')
+            console.log('\n========= MOCK CLIENT CONNECTING... ==========\n')
             const ws = new WebSocket("ws://localhost:1337/charger/123abc")  
 
             ws.on('open', function open() {
@@ -27,10 +40,71 @@ module.exports = function ({ ocppInterface, databaseInterfaceCharger, constants,
             ws.on('message', function message(data){
                 parsedData = JSON.parse(data)
                 console.log(parsedData)
+
+                switch(parsedData[c.MESSAGE_TYPE_INDEX]){
+                    case c.CALL:
+                        callSwitchForClientMock(parsedData, ws)
+                        break
+                    
+                    case c.CALL_RESULT:
+                        callResultSwitchForClientMock(parsedData, ws)
+                        break
+                }
+                
             })
             
         } catch (error) {
             console.log(error)
+        }
+    }
+
+    callSwitchForClientMock = function(parsedData, ws){
+        switch(parsedData[c.ACTION_INDEX]){
+            case c.REMOTE_START_TRANSACTION:
+                jsonResponseMessage = func.buildJSONMessage([ 
+                    3,
+                    parsedData[c.UNIQUE_ID_INDEX],
+                    c.REMOTE_START_TRANSACTION,
+                    { 
+                        "status": c.ACCEPTED
+                    } 
+                ])
+
+                ws.send(jsonResponseMessage)
+
+                jsonRequestMessage = func.buildJSONMessage([ 
+                    2,
+                    parsedData[c.UNIQUE_ID_INDEX],
+                    c.START_TRANSACTION,
+                    { 
+                        "connectorId": 1,
+                        "idTag": 1,
+                        "meterStart": 1,
+                        "reservationId": 1,
+                        "timestamp":1234512345124123
+                    } 
+                ])
+
+                ws.send(jsonRequestMessage)
+                break
+
+            case c.REMOTE_STOP_TRANSACTION:
+                
+                break
+
+            case c.RESERVE_NOW:
+                break
+
+            default:
+                console.log('Client message listener switch default case...')
+                break
+        }
+    }
+
+    callResultSwitchForClientMock = function(parsedData, ws){
+        switch(parsedData[c.ACTION_INDEX]){
+            case c.START_TRANSACTION:
+                console.log('Server responded with: ' + parsedData)
         }
     }
 
