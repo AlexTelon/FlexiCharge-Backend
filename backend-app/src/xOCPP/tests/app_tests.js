ad84cd5c90ea5276d43d6805b3e5a49a511a1f07
Daniel <adduser@DESKTOP-40SNMHV.localdomain>
2022-09-27 17:43:23 +0200

    removed app_test file

--- backend-app/src/xOCPP/tests/app_tests.js
+++
@@ -1,9 +0,0 @@
-module.exports = function ({ ocppInterface, databaseInterfaceCharger, constants}) {
-    const c = constants.get()   
-
-    exports.testStartLivestreaming = function(){
-        
-    }
-
-    return exports
-}
