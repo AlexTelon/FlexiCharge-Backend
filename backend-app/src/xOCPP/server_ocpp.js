52fd94ef07977a48139aba51b951fffabb5bc510
joelAndreasson <joelaron007@gmail.com>
2022-09-28 11:32:33 +0200

    Fixed issues from merge

--- backend-app/src/xOCPP/server_ocpp.js
+++ backend-app/src/xOCPP/server_ocpp.js
@@ -47,6 +47,7 @@ module.exports = function ({ chargerClientHandler, v, databaseInterfaceCharger, 
                         }
                     })
                     break
+            }
         })
 
         
