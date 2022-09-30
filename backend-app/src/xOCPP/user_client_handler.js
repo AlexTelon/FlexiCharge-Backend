eb75ccff0b5ae42dc23c090b962d4dee03d25dd2
fiallos00 <fida20dx@student.ju.se>
2022-09-30 13:09:19 +0200

    Removed user client validation

--- backend-app/src/xOCPP/user_client_handler.js
+++ backend-app/src/xOCPP/user_client_handler.js
@@ -2,31 +2,11 @@ module.exports = function ({ constants, func, broker, v }) {
     const c = constants.get()
 
     exports.handleClient = function (clientSocket, userID) {
-        if(!isValidClient(userID)){
-            const jsonErrorMessage = func.buildJSONMessage([
-                c.CALL_ERROR,
-                420,
-                c.SECURITY_ERROR,
-                'userID is invalid',
-                {}
-            ])
-            clientSocket.send(jsonErrorMessage)
-            clientSocket.terminate()
-            return
-        }
-
         v.addConnectedUserSocket(userID, clientSocket)
         broker.subcribeToLiveMetrics(userID)
         
         console.log("User client with ID: " + userID + " connected to the system.")
         console.log("Number of connected user clients: " + v.getLengthConnectedUserSockets())
-    }
-
-    isValidClient = function (userID){
-        //TODO: Implement!!!
-        //Probably some form of authentication using Cognito
-
-        return true
     }
 
     return exports
