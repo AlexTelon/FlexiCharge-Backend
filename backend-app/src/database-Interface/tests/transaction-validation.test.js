04b00dc6ca8d163e8c03217128bf8655bf875136
Daniel <danielpelmas@hotmail.com>
2022-10-04 14:31:48 +0200

    Finished work with unit tests for transaction validation

--- backend-app/src/database-Interface/tests/transaction-validation.test.js
+++ backend-app/src/database-Interface/tests/transaction-validation.test.js
@@ -47,3 +47,83 @@ describe('Update Charger by chargeMeterValue input', () => {
     });
 
 });
+
+describe('Update Charger by currentChargePercentage input', () => {
+    
+    test('null', () => {
+        const errors = transactionValidation.getUpdateTransactionChargingStatus(validChargeMeterInput,null);
+        expect(errors.length).toBe(1);
+    });
+
+    test('undefined', () => {
+        const valErrors = transactionValidation.getUpdateTransactionChargingStatus(validChargeMeterInput,undefined);
+        expect(valErrors.length).toBe(1);
+    });
+
+    test('integer below 0 string input', () => {
+        const errors = transactionValidation.getUpdateTransactionChargingStatus(validChargeMeterInput,"test");
+        expect(errors.length).toBe(1);
+    });
+
+    test('integer below 0 array input', () => {
+        const errors = transactionValidation.getUpdateTransactionChargingStatus(validChargeMeterInput,[1,2,3]);
+        expect(errors.length).toBe(1);
+    });
+
+    test('integer below 0', () => {
+        const errors = transactionValidation.getUpdateTransactionChargingStatus(validChargeMeterInput,-1);
+        expect(errors.length).toBe(1);
+    });
+
+    test('integer over 100', () => {
+        const errors = transactionValidation.getUpdateTransactionChargingStatus(validChargeMeterInput,101);
+        expect(errors.length).toBe(1);
+    });
+
+});
+
+describe('Test klarna by session_id input', () => {
+    
+    test('null', () => {
+        const errors = transactionValidation.addKlarnaTransactionValidation(null, validClientId);
+        expect(errors.length).toBe(1);
+    });
+
+    test('undefined', () => {
+        const errors = transactionValidation.addKlarnaTransactionValidation(undefined, validClientId);
+        expect(errors.length).toBe(1);
+    });
+
+    test('datatype not string', () => {
+        const errors = transactionValidation.addKlarnaTransactionValidation(1, validClientId);
+        expect(errors.length).toBe(1);
+    });
+
+    test('empty string', () => {
+        const errors = transactionValidation.addKlarnaTransactionValidation("", validClientId);
+        expect(errors.length).toBe(1);
+    });
+})
+
+describe('Test klarna by client_id input', () => {
+    
+    test('null', () => {
+        const errors = transactionValidation.addKlarnaTransactionValidation(validSessionId, null);
+        expect(errors.length).toBe(1);
+    });
+
+    test('undefined', () => {
+        const errors = transactionValidation.addKlarnaTransactionValidation(validSessionId, undefined);
+        expect(errors.length).toBe(1);
+    });
+
+    test('datatype not string', () => {
+        const errors = transactionValidation.addKlarnaTransactionValidation(validSessionId, 1);
+        expect(errors.length).toBe(1);
+    });
+
+    test('empty string', () => {
+        const errors = transactionValidation.addKlarnaTransactionValidation(validSessionId, "");
+        expect(errors.length).toBe(1);
+    });
+});
