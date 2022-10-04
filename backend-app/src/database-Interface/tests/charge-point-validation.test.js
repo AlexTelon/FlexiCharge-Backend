df6a87a13f85def9b851e67502cca6471a4436ab
Lasse Lejon <lasse.lejon@hotmail.com>
2022-10-04 16:29:58 +0200

    Added new test cases.

--- backend-app/src/database-Interface/tests/charge-point-validation.test.js
+++ backend-app/src/database-Interface/tests/charge-point-validation.test.js
@@ -1,7 +1,10 @@
-const chargePointValidation = require("../validation/chargePointValidation")({});
+const chargePointValidation = require("../validation/chargePointValidation")(
+  {}
+);
 const { describe, expect, test } = require("@jest/globals");
 const VALID_LONGITUDE = 120;
 const VALID_LATITUDE = 20;
+const VALID_LOCATION = [VALID_LATITUDE, VALID_LONGITUDE];
 
 describe("Charge Point Validation with input:", () => {
   test("empty strings", () => {
@@ -9,13 +12,44 @@ describe("Charge Point Validation with input:", () => {
     expect(errors.length).toBe(3);
   });
   test("valid input", () => {
-    const location = [VALID_LATITUDE, VALID_LONGITUDE]
-    const errors = chargePointValidation.chargePointValidation("tst", location, 2);
+    const errors = chargePointValidation.chargePointValidation(
+      "tst",
+      VALID_LOCATION,
+      2
+    );
     expect(errors.length).toBe(0);
   });
   test("location array length 3", () => {
-    const location = [VALID_LATITUDE, VALID_LONGITUDE, 123]
-    const errors = chargePointValidation.chargePointValidation("tst", location, 2);
+    const location = [20, 20, 20];
+    const errors = chargePointValidation.chargePointValidation(
+      "tst",
+      location,
+      2
+    );
+    expect(errors.length).toBe(1);
+  });
+  test("price null", () => {
+    const errors = chargePointValidation.chargePointValidation(
+      "tst",
+      VALID_LOCATION,
+      null
+    );
+    expect(errors.length).toBe(1);
+  });
+  test("price undefined", () => {
+    const errors = chargePointValidation.chargePointValidation(
+      "tst",
+      VALID_LOCATION,
+      undefined
+    );
+    expect(errors.length).toBe(1);
+  });
+  test("negative price", () => {
+    const errors = chargePointValidation.chargePointValidation(
+      "tst",
+      VALID_LOCATION,
+      -24
+    );
     expect(errors.length).toBe(1);
   });
 });
