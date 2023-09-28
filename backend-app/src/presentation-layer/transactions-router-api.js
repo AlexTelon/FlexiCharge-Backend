48482bdfe8128bd3b38aea1be215ff91fc7f5a08
loan21xw <102791814+loan21xw@users.noreply.github.com>
2023-09-28 16:34:48 +0200

    Remove unused transaction endpoints

--- backend-app/src/presentation-layer/transactions-router-api.js
+++ backend-app/src/presentation-layer/transactions-router-api.js
@@ -7,7 +7,7 @@ module.exports = function ({ databaseInterfaceTransactions }) {
     function getMockTransaction() {
         return {
             "transactionID": 9999,
-            "isKlarnaPayment": false,
+            "isKlarnaPayment": true,
             "kwhTransfered": Math.floor(Math.random() * 100) + 1, // Random number between 0 and 100
             "currentChargePercentage": Math.floor(Math.random() * 101), // Random number between 0 and 100
             "pricePerKwh": (Math.random() * 100).toFixed(2), // Random number between 0 and 100 with 2 decimal places
@@ -23,6 +23,51 @@ module.exports = function ({ databaseInterfaceTransactions }) {
     }
 
     const router = express.Router()
+
+    router.post('/', function (request, response) {
+
+        const { userID, chargerID, isKlarnaPayment, pricePerKwh } = request.body;
+        if (chargerID == 100000) {
+            const data = getMockTransaction();
+            response.status(201).json(data);
+            return;
+        }
+        databaseInterfaceTransactions.addTransaction(userID, chargerID, isKlarnaPayment, pricePerKwh, function (errors, transactionID) {
+            if (errors.length > 0) {
+                response.status(400).json(errors)
+            } else if (transactionID) {
+                response.status(201).json({
+                    "transactionID": transactionID
+                })
+            } else {
+                response.status(500).json(errors)
+            }
+        })
+    })
+
+    router.put('/start/:transactionID', function (request, response) {
+
+        const transactionID = request.params.transactionID
+        const authorization_token = request.body.authorization_token;
+
+        if (transactionID == 9999) {
+            const data = getMockTransaction();
+            response.status(200).json(data);
+            return;
+        }
+        databaseInterfaceTransactions.createKlarnaOrder(transactionID, authorization_token, function (error, klarnaOrder) {
+            console.log(error);
+            console.log(klarnaOrder);
+            if (error.length === 0) {
+                response.status(200).json(klarnaOrder)
+            } else if (error.includes("internalError") || error.includes("dbError")) {
+                response.status(500).json(error)
+            } else {
+                response.status(400).json(error);
+            }
+        })
+    })
+
     router.get('/:transactionID', function (request, response) {
 
         const transactionID = request.params.transactionID;
@@ -39,6 +84,25 @@ module.exports = function ({ databaseInterfaceTransactions }) {
                 response.status(200).json(transaction)
             } else {
                 response.status(500).json(errors)
+            }
+        })
+    })
+
+    router.put('/stop/:transactionID', function (request, response) {
+        const transactionID = request.params.transactionID
+
+        if (transactionID == 9999) {
+            const data = getMockTransaction();
+            response.status(200).json(data);
+            return;
+        }
+        databaseInterfaceTransactions.finalizeKlarnaOrder(transactionID, function (error, stoppedTransaction) {
+            if (error.length > 0) {
+                response.status(400).json(error)
+            } else if (stoppedTransaction) {
+                response.status(200).json(stoppedTransaction)
+            } else {
+                response.status(500).json(error)
             }
         })
     })
@@ -68,104 +132,6 @@ module.exports = function ({ databaseInterfaceTransactions }) {
                 response.status(200).json(chargerTransaction)
             } else {
                 response.status(500).json(errors)
-            }
-        })
-    })
-
-    router.post('/', function (request, response) {
-
-        const { userID, chargerID, isKlarnaPayment, pricePerKwh } = request.body;
-        if (chargerID == 100000) {
-            const data = getMockTransaction();
-            response.status(200).json(data);
-            return;
-        }
-        databaseInterfaceTransactions.addTransaction(userID, chargerID, isKlarnaPayment, pricePerKwh, function (errors, transactionID) {
-            if (errors.length > 0) {
-                response.status(400).json(errors)
-            } else if (transactionID) {
-                response.status(201).json({
-                    "transactionID": transactionID
-                })
-            } else {
-                response.status(500).json(errors)
-            }
-        })
-    })
-
-
-    router.put('/payment/:transactionID', function (request, response) {
-
-        const transactionID = request.params.transactionID
-        const paymentID = request.body.paymentID
-        databaseInterfaceTransactions.updateTransactionPayment(transactionID, paymentID, function (error, updatedTransactionPayment) {
-            if (error.length == 0) {
-                response.status(201).json(updatedTransactionPayment)
-            } else {
-                if (error.includes("internalError") || error.includes("dbError")) {
-                    response.status(500).json(error)
-                } else {
-                    response.status(404).json(error)
-                }
-            }
-        })
-    })
-
-    router.put('/chargingStatus/:transactionID', function (request, response) {
-        const transactionID = request.params.transactionID
-        const kwhTransfered = request.body.kwhTransfered
-        const currentChargePercentage = request.body.currentChargePercentage
-        databaseInterfaceTransactions.updateTransactionChargingStatus(transactionID, kwhTransfered, currentChargePercentage, function (error, updatedTransaction) {
-            if (error.length == 0) {
-                response.status(201).json(updatedTransaction)
-            } else {
-                if (error.includes("internalError") || error.includes("dbError")) {
-                    response.status(500).json(error)
-                } else {
-                    response.status(404).json(error)
-                }
-            }
-        })
-    })
-
-    router.put('/start/:transactionID', function (request, response) {
-
-        const transactionID = request.params.transactionID
-        const authorization_token = request.body.authorization_token;
-
-        if (transactionID == 9999) {
-            const data = getMockTransaction();
-            response.status(200).json(data);
-            return;
-        }
-        databaseInterfaceTransactions.createKlarnaOrder(transactionID, authorization_token, function (error, klarnaOrder) {
-            console.log(error);
-            console.log(klarnaOrder);
-            if (error.length === 0) {
-                response.status(200).json(klarnaOrder)
-            } else if (error.includes("internalError") || error.includes("dbError")) {
-                response.status(500).json(error)
-            } else {
-                response.status(400).json(error);
-            }
-        })
-    })
-
-    router.put('/stop/:transactionID', function (request, response) {
-        const transactionID = request.params.transactionID
-
-        if (transactionID == 9999) {
-            const data = getMockTransaction();
-            response.status(200).json(data);
-            return;
-        }
-        databaseInterfaceTransactions.finalizeKlarnaOrder(transactionID, function (error, stoppedTransaction) {
-            if (error.length > 0) {
-                response.status(400).json(error)
-            } else if (stoppedTransaction) {
-                response.status(200).json(stoppedTransaction)
-            } else {
-                response.status(500).json(error)
             }
         })
     })
