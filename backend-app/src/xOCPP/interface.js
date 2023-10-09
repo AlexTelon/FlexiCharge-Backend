36732962b4c0bdf44e241b24753d10aabae3e294
SweOlle <80258629+SweOlle@users.noreply.github.com>
2023-10-09 19:01:03 +0200

    OCPP Changes. OCPP Server now registers when a charger is connected. Belongs to issue #211.

--- backend-app/src/xOCPP/interface.js
+++ backend-app/src/xOCPP/interface.js
@@ -9,8 +9,7 @@ module.exports = function ({ v, constants, interfaceHandler, func }) {
         v.addTransactionID(connectorID, transactionID)
 
         const payload = {
-            //is set to one as we assume there is only one connector per charger
-            connectorID: 1,
+            connectorID: connectorID,
             //we don't have users so always set to 1
             idTag: 1,
         }
@@ -19,7 +18,6 @@ module.exports = function ({ v, constants, interfaceHandler, func }) {
 
     exports.remoteStopTransaction = function (connectorID, transactionID, callback) {
 
-        //to do, not working (EDIT 2022-09-22: Klarna in DAL is not working, therefor this is not working)
         console.log("Incoming request from API: stopTransaction -> transactionId;" + transactionID + " connectorID;" + connectorID)
 
         const payload = {
@@ -32,7 +30,7 @@ module.exports = function ({ v, constants, interfaceHandler, func }) {
     //Need a callback function with the parameters "response" and "error", it will callback when the server got a response from the charger.
     //The response have 1 error as of now, "InvalidId". if error occur the response will be null. If no errors occours the erro will be null.
     //If the charger accepts the reservation the response will be "Accepted" if the reservation went through
-    //othervise it will be one of this responses: "Faulted", "Occupied", "Rejected" or "Unavailable".
+    //otherwise it will be one of this responses: "Faulted", "Occupied", "Rejected" or "Unavailable".
     exports.reserveNow = function (connectorID, idTag, parentIdTag, callback) {
 
         console.log("Incoming request from API: reserveNow -> connectorID:" + connectorID)
